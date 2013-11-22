4ad1a48d617b10d6612c16081b3ea4a637862620
tianshan20081 <zhangshch2008@gmail.com>
2013-11-22 09:36:53 +0800

    1.创建 多控制请求类 MultiController .并且增加方法 public ModelAndView add(HttpServletRequest request,HttpServletResponse response){return new ModelAndView()} public ModelAndView map(HttpServletRequest request,HttpServletResponse response){return new ModelAndView("/map","map",new HashMap())}
    2.修改spring-servlet.xml
    添加静态资源(CSS,JQuery,Image...)非过滤列表 <mvc:resources location="/img/**"
    mapping="/img/**">
    3.增加Ognl jar包。
    
    4.导入 jquery-2.0.3.js 支持
    
    5.新建 map.jsp 来获取服务器返回来的数据。并且进行遍历。
    eg : <c:forEach items="${map}" var="val">
    		${val.key}--->${val.value}<br>
    	</c:forEach>
    	<img src="img/img.png">

--- src/com/aoeng/smvc/controller/MultiController.java
+++ src/com/aoeng/smvc/controller/MultiController.java
@@ -3,6 +3,9 @@
  */
 package com.aoeng.smvc.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,5 +22,15 @@ public class MultiController extends MultiActionController
 	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("---------MultiController-------------");
 		return new ModelAndView("multi");
+	}
+	
+	public ModelAndView map(HttpServletRequest request,HttpServletResponse response){
+		System.out.println("---------Map---------------");
+		Map map = new HashMap();
+		map.put("key1", "value1");
+		map.put("key2", "value2");
+		map.put("key3", "value3");
+		map.put("key4", "value4");
+		return new ModelAndView("/map","map",map);
 	}
 }
